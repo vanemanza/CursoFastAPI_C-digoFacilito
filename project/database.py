42b5eafae0b7c75069ac50eda96af35ad61b58c2
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-12-10 14:39:05 -0300

    configuración de la conexion con la bd

---
+++ project/database.py
@@ -0,0 +1,7 @@
+from peewee import *
+
+database = MySQLDatabase('fastapi_project',
+                        user='root',
+                        password='2022Vane$',
+                        host='localhost',
+                        port=3306)
