450eaf77b6e5470f193af670e8d6586a723fd642
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-12-11 11:22:24 -0300

    agrego validación

--- project/schemas.py
+++ project/schemas.py
@@ -1,5 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 class UserBaseModel(BaseModel):
     username : str
     password : str
+
+    @validator('username')
+    def username_validator(cls, username):
+        if len(username) < 3 or len(username) > 50:
+            raise ValueError('El nombre de usuario debe tener mas de 3 y menos de 50 caracteres.')
+
+        return username    
