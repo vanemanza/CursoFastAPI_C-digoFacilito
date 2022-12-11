c612cfe401c19db0219ca09e8285284057ba8b04
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-12-10 23:31:03 -0300

    cambio los args de la bd por variables de entorno

--- project/database.py
+++ project/database.py
@@ -1,7 +1,49 @@
 from peewee import *
+from datetime import datetime
+from decouple import config
 
-database = MySQLDatabase('fastapi_project',
-                        user='root',
-                        password='2022Vane$',
-                        host='localhost',
-                        port=3306)
+database = MySQLDatabase(config('MYSQL_DB'),
+                       user=config('MYSQL_USER'), 
+                       password=config('MYSQL_PASSWORD'), 
+                       host=config('MYSQL_HOST'), 
+                       port=int(config('MYSQL_PORT')))
+class User(Model):
+    username = CharField(max_length=50, unique=True)
+    password = CharField(max_length=50)
+    created_at = DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        database = database
+        table_name = 'users'
+
+class Movie(Model):
+    title = CharField(max_length=50)
+    created_at = DateTimeField(datetime.now)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        database = database
+        table_name = 'movies'
+
+
+class UserReview(Model):
+    user = ForeignKeyField(User, backref='reviews')
+    movie = ForeignKeyField(Movie, backref='reviews')
+    review = TextField()
+    score = IntegerField()
+    created_at = DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.movie.title}'
+
+    class Meta:
+        database = database
+        table_name = 'users_reviews'
+
+
+
