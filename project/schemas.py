4880872a6a62fbbdf8207e77e6755c4c4cc386f4
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-12-12 00:00:17 -0300

    clase para serializar objetos - metodo get

--- project/schemas.py
+++ project/schemas.py
@@ -1,5 +1,21 @@
 from pydantic import BaseModel, validator
 
+from pydantic.utils import GetterDict
+
+from typing import Any
+
+from peewee import ModelSelect
+
+class PeeweeGetterDict(GetterDict):
+    def get(self, key: Any, default: Any = None):
+        
+        res = getattr(self._obj, key, default)
+
+        if isinstance(res, ModelSelect):
+            return list(res)
+
+        return res    
+
 class UserRequestModel(BaseModel):
     username : str
     password : str
@@ -14,6 +30,12 @@ class UserRequestModel(BaseModel):
 class UserResponseModel(BaseModel):
     id: int
     username: str
+
+    class Config:
+        orm_mode = True
+        getter_dict = PeeweeGetterDict
+
+
 
     
 
